ab22a87a2fcacb6afee961c43dc26c91878c9279
Maria <mlovatonv@gmail.com>
2021-05-29 16:40:54 -0500

    Write tree

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <utility>
 #include <vector>
@@ -120,6 +121,26 @@ public:
         }
     }
 
+    vector<QuadTree *> get_children() const
+    {
+        return this->children;
+    }
+
+    friend ostream &operator<<(ostream &output, const QuadTree &o)
+    {
+        output << o.bbox.bottom_left.x << " "
+               << o.bbox.bottom_left.y << " "
+               << o.bbox.top_right.x << " "
+               << o.bbox.top_right.y << " ";
+        for (auto &point : o.points)
+        {
+            output << point.x << " "
+                   << point.y << " "
+                   << point.data;
+        }
+        return output;
+    }
+
     bool is_leaf() const
     {
         return children.empty();
@@ -219,6 +240,38 @@ struct IO
 {
     void write_tree(string filename, QuadTree tree)
     {
+        ofstream file(filename, ios::out); // | ios::binary
+        assert(file);
+
+        int idx = 0;
+        queue<QuadTree *> q;
+        q.push(&tree);
+
+        while (!q.empty())
+        {
+            QuadTree t = *q.front();
+            q.pop();
+            if (!t.is_leaf())
+            {
+                for (QuadTree *child : t.get_children())
+                {
+                    q.push(child);
+                }
+                for (int i = idx + 1; i <= idx + 4; ++i)
+                {
+                    file << i << " ";
+                }
+                idx += 4;
+            }
+            else
+            {
+                for (int i = 0; i < 4; ++i)
+                {
+                    file << "-1 ";
+                }
+            }
+            file << t << endl;
+        }
     }
 
     QuadTree read_tree(string filename)
@@ -232,7 +285,7 @@ struct IO
 
     vector<XY> read_image(string filename)
     {
-        ifstream file(filename);
+        ifstream file(filename, ios::in);
         assert(file);
 
         string line;
@@ -251,6 +304,8 @@ struct IO
             }
         }
 
+        file.close();
+
         return points;
     }
 } io;
@@ -265,9 +320,9 @@ int main()
     {
         tree.insert(point);
     }
-    tree.print();
+    // tree.print();
 
-    // io.write_tree("tree", tree);
+    io.write_tree("tree", tree);
 
     // io.write_image("image.pgm", io.read_tree("tree"));
 
